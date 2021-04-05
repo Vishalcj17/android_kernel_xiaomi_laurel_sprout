d2911915440483bb9fc8b62e75952be883e855f9
Vishalcj17 <vishalcj@aospa.co>
2021-04-05 15:50:56 +0530

    msm_vidc: Gaurd DEBUG_FS code to avoid spam.
    - it's not me, it's CAF at fault.
    
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: I2296b7db7eb2a01da7c732b3ddb5998939ffd27b

--- drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
+++ drivers/media/platform/msm/vidc/msm_v4l2_vidc.c
@@ -623,9 +623,10 @@ static int msm_vidc_probe_vidc_device(struct platform_device *pdev)
 	list_add_tail(&core->list, &vidc_driver->cores);
 	mutex_unlock(&vidc_driver->lock);
 
+#ifdef CONFIG_DEBUG_FS
 	core->debugfs_root = msm_vidc_debugfs_init_core(
 		core, vidc_driver->debugfs_root);
-
+#endif
 	vidc_driver->sku_version = core->resources.sku_version;
 
 	dprintk(VIDC_DBG, "populating sub devices\n");
@@ -900,11 +901,11 @@ static int __init msm_vidc_init(void)
 
 	INIT_LIST_HEAD(&vidc_driver->cores);
 	mutex_init(&vidc_driver->lock);
+#ifdef CONFIG_DEBUG_FS
 	vidc_driver->debugfs_root = msm_vidc_debugfs_init_drv();
 	if (!vidc_driver->debugfs_root)
 		dprintk(VIDC_ERR,
 			"Failed to create debugfs for msm_vidc\n");
-
 	rc = platform_driver_register(&msm_vidc_driver);
 	if (rc) {
 		dprintk(VIDC_ERR,
@@ -913,7 +914,7 @@ static int __init msm_vidc_init(void)
 		kfree(vidc_driver);
 		vidc_driver = NULL;
 	}
-
+#endif
 	return rc;
 }
 
